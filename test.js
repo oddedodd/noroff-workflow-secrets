dd1a3f8e0437ebaa84479193799c4b9312a64830
Odd Egil Hegge Selnes <post@odde.org>
2024-10-20 13:12:41 +0200

    Added test

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 require("dotenv/config");
+
+function test(expectedSecret) {
+  const secretMatches = process.env.MEANING_OF_LIFE === expectedSecret;
+  console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
+  console.log(
+    `The secret does${secretMatches ? "" : " not"} match the expected secret`
+  );
+  return secretMatches;
+}
+test("42");
